2662821864818373d536883c84754583400f3c47
ReshmaGhanta <reshma.ghanta@gmail.com>
2022-10-11 00:21:04 -0400

    Updates

---
+++ precache-manifest.b72fbbd9a7e0fa197749ddb5919af657.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbcdf1d9a273ce31e1d8",
+    "url": "/Reshma-Portfolio/static/css/main.8452bfb2.chunk.css"
+  },
+  {
+    "revision": "fbcdf1d9a273ce31e1d8",
+    "url": "/Reshma-Portfolio/static/js/main.a98b21e1.chunk.js"
+  },
+  {
+    "revision": "6e99421db894119b069b",
+    "url": "/Reshma-Portfolio/static/js/runtime~main.701ce47c.js"
+  },
+  {
+    "revision": "35263af7a3cf57f47e8e",
+    "url": "/Reshma-Portfolio/static/js/2.697afbe6.chunk.js"
+  },
+  {
+    "revision": "e9992e6f40e61fdf273698e9aae0da11",
+    "url": "/Reshma-Portfolio/static/media/Logo.e9992e6f.png"
+  },
+  {
+    "revision": "30e97e5ce2102b204e4490782c4ec6e0",
+    "url": "/Reshma-Portfolio/static/media/ReshmaResume.30e97e5c.pdf"
+  },
+  {
+    "revision": "278cc939422c861f5371a49a785363ae",
+    "url": "/Reshma-Portfolio/static/media/MasonLogo.278cc939.png"
+  },
+  {
+    "revision": "f9d3895425a082e07f71b8c1a37fb754",
+    "url": "/Reshma-Portfolio/index.html"
+  }
+];
