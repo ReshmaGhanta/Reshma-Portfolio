e632a21a3960ca905723e70e0ebd41a423d88032
ReshmaGhanta <reshma.ghanta@gmail.com>
2023-08-16 14:02:33 -0400

    Updates

---
+++ precache-manifest.01e2a75821018d55d52e770395ec83ce.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64baf2728ad9f535fc6cbf49865cee21",
+    "url": "/Reshma-Portfolio/index.html"
+  },
+  {
+    "revision": "73c8b762a3bc84fea089",
+    "url": "/Reshma-Portfolio/static/css/main.650add93.chunk.css"
+  },
+  {
+    "revision": "d86259deba71a8fb729c",
+    "url": "/Reshma-Portfolio/static/js/2.a38b0576.chunk.js"
+  },
+  {
+    "revision": "e3887469d9551fbbdc76a86b58bab6fe",
+    "url": "/Reshma-Portfolio/static/js/2.a38b0576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73c8b762a3bc84fea089",
+    "url": "/Reshma-Portfolio/static/js/main.197262bc.chunk.js"
+  },
+  {
+    "revision": "3b9b654bd2cc8c9fa65e",
+    "url": "/Reshma-Portfolio/static/js/runtime-main.b57d9bfe.js"
+  },
+  {
+    "revision": "69db22b54b3b35964664e692162abc6d",
+    "url": "/Reshma-Portfolio/static/media/ExpressJs.69db22b5.png"
+  },
+  {
+    "revision": "22f83cfd5c3ae79b2ff732911c75d946",
+    "url": "/Reshma-Portfolio/static/media/Html.22f83cfd.png"
+  },
+  {
+    "revision": "6067f8f0912af0c606001c530e145c42",
+    "url": "/Reshma-Portfolio/static/media/Javascript.6067f8f0.png"
+  },
+  {
+    "revision": "91a2e891206f38532f7ada39a1e6b1e0",
+    "url": "/Reshma-Portfolio/static/media/Logo.91a2e891.png"
+  },
+  {
+    "revision": "278cc939422c861f5371a49a785363ae",
+    "url": "/Reshma-Portfolio/static/media/MasonLogo.278cc939.png"
+  },
+  {
+    "revision": "e98f8d88c4f02bc1b39256b37c06d11a",
+    "url": "/Reshma-Portfolio/static/media/PvpsitLogo.e98f8d88.png"
+  },
+  {
+    "revision": "21c252593c1c2b8ae1e1f20e3e723996",
+    "url": "/Reshma-Portfolio/static/media/React.21c25259.png"
+  },
+  {
+    "revision": "30e97e5ce2102b204e4490782c4ec6e0",
+    "url": "/Reshma-Portfolio/static/media/ReshmaResume.30e97e5c.pdf"
+  },
+  {
+    "revision": "d90f8f94d74fcc66aeb7bd52d8ec4094",
+    "url": "/Reshma-Portfolio/static/media/css.d90f8f94.png"
+  },
+  {
+    "revision": "d4bfe0469b086ecd9b7e50073955e23e",
+    "url": "/Reshma-Portfolio/static/media/firebase.d4bfe046.png"
+  },
+  {
+    "revision": "d39410b2eceb338fb93bc8bdc7b7a53f",
+    "url": "/Reshma-Portfolio/static/media/jQuery.d39410b2.png"
+  },
+  {
+    "revision": "364126d3797a9e73918f5ac12f7575c7",
+    "url": "/Reshma-Portfolio/static/media/java.364126d3.png"
+  },
+  {
+    "revision": "939337523ba326326913e391fa581b13",
+    "url": "/Reshma-Portfolio/static/media/nodejs.93933752.png"
+  }
+]);
